03e1fb295c7f0fb27c6cac37397453a1dd18922b
이동준 <scvzl@naver.com>
2023-04-27 14:24:57 +0900

    Fix) spelling

--- Python 배우기(1~50)/2525.py
+++ Python 배우기(1~50)/2525.py
@@ -2,5 +2,5 @@ a, b = map(int, input().split())
 c = int(input())
 total = a*60 + b + c
 hour = total//60%24
-minuite = total%60
-print(hour, minuite)
+minute = total%60
+print(hour, minute)
--- Python 배우기(1~50)/2530.py
+++ Python 배우기(1~50)/2530.py
@@ -2,6 +2,6 @@ a, b, c = map(int, input().split())
 d = int(input())
 total = a*60*60 + b*60 + c + d
 hour = total//60//60%24
-minuite = total//60%60
+minute = total//60%60
 second = total%60
-print(hour, minuite, second)
+print(hour, minute, second)
