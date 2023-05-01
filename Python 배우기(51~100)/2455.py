7237795272fb1582e9147ef7230963a0fe63730a
이동준 <scvzl@naver.com>
2023-05-01 15:17:04 +0900

    Create 2455.py

---
+++ Python 배우기(51~100)/2455.py
@@ -0,0 +1,5 @@
+people_list = [0]
+for _ in range(4):
+    out_people, in_people = map(int, input().split())
+    people_list.append(people_list[-1] + in_people - out_people)
+print(max(people_list))
