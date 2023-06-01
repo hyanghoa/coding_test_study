8d3bb7ffade9ab85b763aedd5c03cce4f15b06d1
이동준 <scvzl@naver.com>
2023-06-01 17:30:57 +0900

    Update 21944.py

--- Data Structure2 (자료구조2)/21944.py
+++ Data Structure2 (자료구조2)/21944.py
@@ -6,16 +6,17 @@ from collections import defaultdict
 class Solution:
     def __init__(self):
         self.workbook_info = {}
-        self.algorithm = defaultdict({"min": list, "max": list})
-        self.level = defaultdict({"min": list, "max": list})
+        self.min_algorithm = defaultdict(list)
+        self.max_algorithm = defaultdict(list)
+        self.min_level = defaultdict(list)
+        self.max_level = defaultdict(list)
 
     def recommend(self, g, x):
         g, x = int(g), int(x)
         algorithm = self.max_algorithm[g] if x == 1 else self.min_algorithm[g]
         while algorithm:
             recommend_l, recommend_p = abs(algorithm[0][0]), abs(algorithm[0][1])
-            info = self.workbook_info.get(recommend_p)
-            if info is not None and info[0] == recommend_l and info[1] == g:
+            if self.workbook_info.get(recommend_p) is not None and self.workbook_info[recommend_p][0] == recommend_l and self.workbook_info[recommend_p][1] == g:
                 print(recommend_p)
                 break
             else:
@@ -23,50 +24,75 @@ class Solution:
 
     def recommend2(self, x):
         x = int(x)
-        key = "max" if x == 1 else "min"
-        while self.level[key]:
-            level = max(self.level[key]) if x == 1 else min(self.level[key])
-            while self.level[key][level]:
-                p, g = abs(self.level[key][level][0][0]), abs(self.level[key][level][0][1])
-                info = self.workbook_info.get(p)
-                if info is not None and info[0] == level and info[1] == g:
-                    print(p)
-                    return
-                else:
-                    heapq.heappop(self.level[key][level])
-            self.level.pop(level)
+        if x == 1:
+            while self.max_level:
+                max_level = max(self.max_level)
+                while self.max_level[max_level]:
+                    p, g = -self.max_level[max_level][0][0], -self.max_level[max_level][0][1]
+                    if self.workbook_info.get(p) is not None and self.workbook_info[p][0] == max_level and self.workbook_info[p][1] == g:
+                        print(p)
+                        return
+                    else:
+                        heapq.heappop(self.max_level[max_level])
+                self.max_level.pop(max_level)
+        elif x == -1:
+            while self.min_level:
+                min_level = min(self.min_level)
+                while self.min_level[min_level]:
+                    p, g = self.min_level[min_level][0][0], self.min_level[min_level][0][1]
+                    if self.workbook_info.get(p) is not None and self.workbook_info[p][0] == min_level and self.workbook_info[p][1] == g:
+                        print(p)
+                        return
+                    else:
+                        heapq.heappop(self.min_level[min_level])
+                self.min_level.pop(min_level)
 
     def recommend3(self, x, l):
         x, l = int(x), int(l)
-        key = "max" if x == 1 else "min"
-        while self.level[key]:
-            level = 101 if x == 1 else 0
-            for lv in self.level[key]:
-                condition = l <= lv if x == 1 else l > lv
-                if condition:
-                    level = min(level, lv) if x == 1 else max(level, lv)
-            while self.level[key][level]:
-                p, g = abs(self.level[key][level][0][0]), abs(self.level[key][level][0][1])
-                info = self.workbook_info.get(p)
-                if info is not None and info[0] == level and info[1] == g:
-                    print(p)
+        if x == 1:
+            while self.min_level:
+                min_level = 101
+                for ml in self.min_level:
+                    if l <= ml:
+                        min_level = min(min_level, ml)
+                while self.min_level[min_level]:
+                    p, g = self.min_level[min_level][0][0], self.min_level[min_level][0][1]
+                    if self.workbook_info.get(p) is not None and self.workbook_info[p][0] == min_level and self.workbook_info[p][1] == g:
+                        print(p)
+                        return
+                    else:
+                        heapq.heappop(self.min_level[min_level])
+                if min_level == 101:
+                    print(-1)
                     return
                 else:
-                    heapq.heappop(self.level[key][level])
-            init_value = 101 if x == 1 else 0
-            if level == init_value:
-                print(-1)
-                return
-            else:
-                self.level[key].pop(level)
-        
+                    self.min_level.pop(min_level)
+        elif x == -1:
+            while self.max_level:
+                max_level = 0
+                for ml in self.max_level:
+                    if l > ml:
+                        max_level = max(max_level, ml)
+                while self.max_level[max_level]:
+                    p, g = -self.max_level[max_level][0][0], -self.max_level[max_level][0][1]
+                    if self.workbook_info.get(p) is not None and self.workbook_info[p][0] == max_level and self.workbook_info[p][1] == g:
+                        print(p)
+                        return
+                    else:
+                        heapq.heappop(self.max_level[max_level])
+                if max_level == 0:
+                    print(-1)
+                    return
+                else:
+                    self.max_level.pop(max_level)
+
     def add(self, p, l, g):
         p, l, g = int(p), int(l), int(g)
         self.workbook_info[p] = (l, g)
-        heapq.heappush(self.algorithm[g]["min"], (l, p))
-        heapq.heappush(self.algorithm[g]["max"], (-l, -p))
-        heapq.heappush(self.level[l]["min"], (p, g))
-        heapq.heappush(self.level[l]["max"], (-p, -g))
+        heapq.heappush(self.min_algorithm[g], (l, p))
+        heapq.heappush(self.max_algorithm[g], (-l, -p))
+        heapq.heappush(self.min_level[l], (p, g))
+        heapq.heappush(self.max_level[l], (-p, -g))
 
     def solved(self, p):
         p = int(p)
